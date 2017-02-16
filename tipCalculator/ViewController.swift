9ce85babce65d410af79749979ea98d81caceab2
jaugeni <jaugeni@gmail.com>
2017-02-16 11:42:48 -0500

    all done

--- tipCalculator/ViewController.swift
+++ tipCalculator/ViewController.swift
@@ -8,18 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var costLbl: UITextField!
+    @IBOutlet weak var amountLbl: UILabel!
+    @IBOutlet weak var sliderOut: UISlider!
+    @IBOutlet weak var totalLbl: UILabel!
+    
+    var totalCost = Double()
+    var tipAmount = Double()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        costLbl.delegate = self
+        amountLbl.text = String(format: "%0.0f", sliderOut.value) + "%"
+        tipAmount = Double(sliderOut.value)
+    }
+    
+    func calcTotalCost(cost: Double) {
+        let tip = (tipAmount / 100) * cost
+        totalCost = tip + Double(costLbl.text!)!
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+
+        costLbl.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        self.view.endEditing(true)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func sliderAct(_ sender: Any) {
+        tipAmount = Double(sliderOut.value)
+        let tipString = String(format: "%0.0f", tipAmount)
+        tipAmount = Double(tipString)!
+        amountLbl.text = "\(tipString)%"
     }
-
-
+    
+    @IBAction func calculatePressed(_ sender: Any) {
+        if costLbl.text != "" {
+            calcTotalCost(cost: Double(costLbl.text!)!)
+            totalLbl.text = String(format: "%0.2f", totalCost) + "$"
+        } else {
+            totalLbl.text = "\(0)"
+        }
+    }
 }
 
